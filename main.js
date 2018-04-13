e72a71b36aa0bb391d014ffaac259bd3e89a23cb
Henry <hdoan002@ucr.edu>
2018-04-12 21:35:55 -0700

    Adding intial js file

---
+++ main.js
@@ -0,0 +1,16 @@
+var TwitterStream = require("twitter-stream-api"),
+  fs = require("fs");
+
+var keys = {
+  consumer_key: "your_consumer_key",
+  consumer_secret: "your_consumer_secret",
+  token: "your_access_token_key",
+  token_secret: "your_access_token_secret"
+};
+
+var Twitter = new TwitterStream(keys, false);
+Twitter.stream("statuses/filter", {
+  track: "javascript"
+});
+
+Twitter.pipe(fs.createWriteStream("tweets.json"));
